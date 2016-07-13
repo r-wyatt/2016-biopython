6bfbdf2c2536a24257997fb7f0201527addc90f1
r-wyatt <rachael.a.wyatt@gmail.com>
2016-07-13 16:27:51 -0300

    trying MUSCLE

--- align.py
+++ align.py
@@ -7,15 +7,18 @@
 # Set-up section
 #-----------------------------------------------------------------------------
 from Bio.Align.Applications import ClustalwCommandline
+from Bio.Align.Applications import MuscleCommandline
 from Bio import AlignIO
 from Bio import Phylo
+from StringIO import StringIO
+import subprocess
 import sys
 import os
 
 #-----------------------------------------------------------------------------
 # Clustal run
 #-----------------------------------------------------------------------------
-# Todo build code to run clustal (install also)
+'''# Todo build code to run clustal (install also)
 clustalw_exe = r"C:\Program Files\ClustalW2\clustalw2.exe"
 clustalw_cline = ClustalwCommandline(clustalw_exe, infile="results\\opuntia.fasta")
 
@@ -26,8 +29,15 @@ stdout, stderr = clustalw_cline()
 
 tree = Phylo.read("results\\opuntia.dnd","newick")
 Phylo.draw_ascii(tree)
-
+'''
 #-----------------------------------------------------------------------------
 # USE MUSCLE!!! Put code in here:
 #-----------------------------------------------------------------------------
+muscle_cline = MuscleCommandline(input="opuntia.fasta")
+save_stdout = sys.stdout
+save_stderr = sys.stderr
+
+stdout,stderr = muscle_cline()
+align = AlignIO.read(StringIO(stdout), "fasta")
+print(align)
 
