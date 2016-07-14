f4e13f9e561ebcaa15728a0c2f2ab47ab2f581c1
r-wyatt <rachael.a.wyatt@gmail.com>
2016-07-14 15:28:52 -0300

    align and trim alignment

--- align.py
+++ align.py
@@ -1,7 +1,7 @@
 #-----------------------------------------------------------------------------
 # Clustalw run and read
 #-----------------------------------------------------------------------------
-''' This code requires that Clustalw be installed under the directory
+''' This code requires that muscle.exe be added to the PATH
 '''
 #-----------------------------------------------------------------------------
 # Set-up section
@@ -16,28 +16,23 @@ import sys
 import os
 
 #-----------------------------------------------------------------------------
-# Clustal run
+# Function to use trimal to trim alignment
 #-----------------------------------------------------------------------------
-'''# Todo build code to run clustal (install also)
-clustalw_exe = r"C:\Program Files\ClustalW2\clustalw2.exe"
-clustalw_cline = ClustalwCommandline(clustalw_exe, infile="results\\opuntia.fasta")
+def trim_align(alignpath,out):
+	cmd = ['trimal', '-in', alignpath, '-out', out, "-automated1"]
+	process = subprocess.Popen(cmd)
+	process.wait()
+	print('Done')
 
-assert os.path.isfile(clustalw_exe), "Clustal W executable missing"
-save_stdout = sys.stdout
-save_stderr = sys.stderr
-stdout, stderr = clustalw_cline()
-
-tree = Phylo.read("results\\opuntia.dnd","newick")
-Phylo.draw_ascii(tree)
-'''
 #-----------------------------------------------------------------------------
 # USE MUSCLE!!! Put code in here:
 #-----------------------------------------------------------------------------
-muscle_cline = MuscleCommandline(input="opuntia.fasta")
-save_stdout = sys.stdout
-save_stderr = sys.stderr
+out_file = "results\\opuntia_aln.fasta"
+in_file = "results\\opuntia.fasta"
+muscle_cline = MuscleCommandline(input=in_file,out=out_file)
 
-stdout,stderr = muscle_cline()
-align = AlignIO.read(StringIO(stdout), "fasta")
-print(align)
+
+trim_align(out_file,"results\\trim_opuntia.aln")
+
+
 
