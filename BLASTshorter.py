5a46412a015d3405dcac4d55216bac9a54c868ee
r-wyatt <rachael.a.wyatt@gmail.com>
2016-07-15 15:57:44 -0300

    mid-way through tsing

--- BLASTshorter.py
+++ BLASTshorter.py
@@ -55,8 +55,8 @@ def rmdir_p(path):
 	if os.path.isdir(path):
 		rmtree(path)
 
-def cleanup():
-	directories = ['results\\BLAST','results\\accs','results\\fasta']
+def cleanup(dir):
+	directories = [dir,dir+'\\BLAST',dir+'\\accs',dir+'\\fasta']
 
 	for folder in directories:
 		rmdir_p(folder)
@@ -100,7 +100,17 @@ def filter_species(listOfTuples, shortSpecies):
 			filtered.append(each)
 	output = list(set(filtered))
 	return output
-	
+
+#-----------------------------------------------------------------------------
+# Consolidate species files
+#-----------------------------------------------------------------------------
+def consolidate_species(dir):
+	files = [f for f in listdir(dir) if isfile(dir+f)]
+# TODO unfinished script here
+
+
+
+
 #=============================================================================
 #-------- Basic Functionality ------------------------------------------------
 #=============================================================================
@@ -109,11 +119,11 @@ def filter_species(listOfTuples, shortSpecies):
 # Basic BLAST Functionality (searching database given gi and species query)
 #-----------------------------------------------------------------------------
 def search_NCBI(species, seqQuery, out):
-	#print("\n\nNot an operational search just yet, but good job anyway")
+	print("\n\nNot an operational search just yet, but good job anyway")
 	print("\n\nSearching: "+species+" for "+seqQuery)
 	
 	# NCBI query
-	
+	'''
 	eQ = species + '\[Organism\]'
 	gi = seqQuery
 	result = NCBIWWW.qblast("blastp","nr", gi, entrez_query= eQ, expect=0.001, hitlist_size=100, ncbi_gi=True)
@@ -125,7 +135,7 @@ def search_NCBI(species, seqQuery, out):
 	save_file.close()
 	result.close()
 	time.sleep(120) # Pause 2min between database queries
-
+'''
 
 #-----------------------------------------------------------------------------
 # Given input file, give a csv of IDs
@@ -135,9 +145,9 @@ def search_NCBI(species, seqQuery, out):
 # third is the name of the file to write the accessions to, and fourth is the name of the
 # file to send the file names of processed BLAST searches to.
 
-def get_ids(filename, ethresh = 0.01):
+def get_ids(filename, dir, ethresh = 0.01):
 	eValueThresh = ethresh
-	result = open("results\\BLAST\\"+filename) # mode omitted defaults to read only
+	result = open(dir+"\\BLAST\\"+filename) # mode omitted defaults to read only
 	blast_record = NCBIXML.parse(result)
 	blast_records = list(blast_record)
 	record = blast_records[0]
@@ -160,7 +170,7 @@ def get_ids(filename, ethresh = 0.01):
 	# Saving results
 	# Save as separate files for each species~!
 	name = record_name(filename)
-	with open("results\\accs\\"+name+".csv",'w') as csvfile:
+	with open(dir+"\\accs\\"+name+".csv",'w') as csvfile:
 		blasthits = csv.writer(csvfile)
 		for each in filteredHits:
 			blasthits.writerow([each[0]])
@@ -169,10 +179,10 @@ def get_ids(filename, ethresh = 0.01):
 #-----------------------------------------------------------------------------
 # Fetch data can take either the list of accessions or name of csv (one entry per line)
 #-----------------------------------------------------------------------------
-def fetch_data(datatype, rname):
-	accs = csv_to_list("results\\accs\\"+rname+".csv")
+def fetch_data(datatype, rname, dir):
+	accs = csv_to_list(dir+"\\accs\\"+rname+".csv")
 	save_stdout = sys.stdout 
-	sys.stdout = open("results\\fasta\\"+rname+".txt", "w")
+	sys.stdout = open(dir+"\\fasta\\"+rname+".txt", "w")
 	handle = Entrez.efetch(db="protein", id=accs, rettype=datatype, retmode="text")
 	print handle.read()
 	sys.stdout = save_stdout
@@ -184,7 +194,7 @@ def fetch_data(datatype, rname):
 #-----------------------------------------------------------------------------
 # BLAST controller
 #-----------------------------------------------------------------------------	
-def run_blasts():
+def run_blasts(dir):
 	indexfile = raw_input("Enter name of csv file with species and gi numbers: ")
 	if os.path.isfile(indexfile):
 		with open(indexfile,'rb') as directions:
@@ -193,7 +203,7 @@ def run_blasts():
 			for row in index:
 				setDirections.append(row)
 			count = len(setDirections)	
-			with open("results\\filenames.csv",'w') as csvfile:
+			with open(dir+"\\filenames.csv",'w') as csvfile:
 				filenames = csv.writer(csvfile)
 				for i in range(0,count-1):
 					if setDirections[i] != None:
@@ -201,9 +211,9 @@ def run_blasts():
 						species = str(setDirections[i][0])
 						matchSpecies = re.match(r'([A-Z|a-z])[A-Z|a-z]* ([A-Z|a-z]{3}).*', species)
 						shortSpecies = matchSpecies.group(1).upper() + matchSpecies.group(2).lower()
-						if not os.path.exists("results\\BLAST\\"):
-							mkdir_p("results\\BLAST\\")
-						out = "results\\BLAST\\" + shortSpecies + "_" + query + ".xml"
+						if not os.path.exists(dir+"\BLAST\\"):
+							mkdir_p(dir+"\\BLAST\\")
+						out = dir+"\\BLAST\\" + shortSpecies + "_" + query + ".xml"
 						search_NCBI(species,query,out)
 						filenames.writerow((species, out))
 			csvfile.close()	
@@ -224,26 +234,24 @@ def parse_files(dir):
 #-----------------------------------------------------------------------------
 # Run BLAST searches
 #-----------------------------------------------------------------------------
-cleanup()
-run_blasts()
+directoryName = raw_input("Enter directory name to write results to: ")
+print "\n\n Clearing/creating new directory..."
+cleanup(directoryName)
+run_blasts(directoryName)
 
 #-----------------------------------------------------------------------------
 # Run parser and data fetch
 #-----------------------------------------------------------------------------
-
-bin = open("outfile.csv","w")
-bin.close()
-
 print "\n\nFetching genbank data and saving..."	
-parse_files("results\\BLAST\\")
+parse_files(directoryName+"\\BLAST\\")
 
 #-----------------------------------------------------------------------------
 # Reformat genbank file to FASTA
 #-----------------------------------------------------------------------------
 print "\n\nFormatting genbank data to FASTA..."
 
-gbk_filename = "results\\gb_outfile.txt"
-faa_filename = "results\\fasta_outfile.txt"
+gbk_filename = directoryName+"\\gb_outfile.txt"
+faa_filename = directoryName+"\\fasta_outfile.txt"
 input_handle  = open(gbk_filename, "r")
 output_handle = open(faa_filename, "w")
 
