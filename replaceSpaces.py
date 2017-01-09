2bdba0b746660ca0c4cff9afe04f3136e90b186c
R. A. Wyatt <rachael.a.wyatt@unb.ca>
2017-01-09 11:43:39 -0400

    fasta cleaning scripts completed

--- replaceSpaces.py
+++ replaceSpaces.py
@@ -1,8 +1,25 @@
-# Remove spaces in fasta file sequence names
-# Functionally this will replace all spaces with underscores
-# for any line that includes ">"
+#!/usr/bin/env
+#-----------------------------------------------------------------------------
+# Automated BLAST searching
+#-----------------------------------------------------------------------------
+''' Script to clean up fasta files
+Written by R. A. Wyatt in Jan, 2017. sequence_cleaner function from
+<biopython.org/wiki/Sequence_Cleaner>, author unknown.
 
+
+Remove spaces in fasta file sequence names
+Functionally this will replace all spaces with underscores
+for any line that includes ">"
+
+Settings here:
+'''
+minlength = 200 # Minimum length of sequences (shorter will be trimmed)
+
+#-----------------------------------------------------------------------------
+# Set-up section
+#-----------------------------------------------------------------------------
 import re, sys, glob, os
+from Bio import SeqIO
 
 def rem_spaces(dir, file):
 	stdout_bk = sys.stdout
@@ -13,12 +30,44 @@ def rem_spaces(dir, file):
 				line = re.sub(r' ',"_",line)
 			sys.stdout.write(line)
 	sys.stdout = stdout_bk
+	return("ns"+file)
 	
+
+def sequence_cleaner(dir, fasta_file, min_length=0, por_n=100):
+	# Create our hash table to add the sequences
+	sequences={}
+	# Using the Biopython fasta parse we can read our fasta input
+	for seq_record in SeqIO.parse(os.path.join(dir,fasta_file), "fasta"):
+		# Take the current sequence
+		sequence = str(seq_record.seq).upper()
+		# Check if the current sequence is according to the user parameters
+		if (len(sequence) >= min_length and
+			(float(sequence.count("N"))/float(len(sequence)))*100 <= por_n):
+		# If the sequence passed in the test "is it clean?" and it isn't in the
+		# hash table, the sequence and its id are going to be in the hash
+			if sequence not in sequences:
+				sequences[sequence] = seq_record.id
+	   # If it is already in the hash table, we're just gonna concatenate the ID
+	   # of the current sequence to another one that is already in the hash table
+			else:
+				if not re.search(seq_record.id,sequences[sequence]):
+					sequences[sequence] += "_" + seq_record.id
+
+
+	# Write the clean sequences
+
+	# Create a file in the same directory where you ran this script
+	output_file = open(os.path.join(dir,"clear_" + fasta_file), "w+")
+	# Just read the hash table and write on the file as a fasta format
+	for sequence in sequences:
+		output_file.write(">" + sequences[sequence] + "\n" + sequence + "\n")
+	output_file.close()
 
 def do_all_files(directory):
 	dir = os.path.join(directory,"fasta")
 	files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f))]
 	for file in files:
-		rem_spaces(dir,file)
+		newfile = rem_spaces(dir,file)
+		sequence_cleaner(dir,newfile,minlength)
 	
 do_all_files(sys.argv[1])
